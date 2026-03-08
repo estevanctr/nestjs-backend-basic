4c654f8a93456e1e529f3cf9795f175b2950fef5
Estevan Cantuária <estevancantuaria23@gmail.com>
2026-03-08 16:54:09 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DevelopersModule } from './developers/developers.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [],
