f0f2d86e448787145f922fdbba90c449df32fd87
Estevan Cantuária <estevancantuaria23@gmail.com>
2026-03-08 15:54:04 -0300

    Update app.controller.ts
    Add more example endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('test')
+  getHelloTest(): string {
+    return this.appService.getHello();
+  }
+  
+  @Get('test2')
+  getHelloTest2(): string {
     return this.appService.getHello();
   }
 }
